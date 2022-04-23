51679c383f21d05770e1fd7e181156986fa91333
Gustavo Koch <guzko@hotmail.com>
2022-04-23 16:55:55 +0200

    3d commit

---
+++ src/NavSearch.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+export default function NavSearch(props) {
+  return (
+<div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#about">About</a>
+  <a href="#contact">Contact</a>
+  <input type="text" placeholder="Search.."/>
+</div>
+  );
+}
