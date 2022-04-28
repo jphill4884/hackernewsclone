8514deeb3bda81556e9c33d3b4963d7b092b2596
Gustavo Koch <guzko@hotmail.com>
2022-04-28 21:44:10 +0200

    4d commit

--- src/NavSearch.js
+++ src/NavSearch.js
@@ -1,14 +1,38 @@
 import React from "react";
+import "./NavSearch.css";
 
 
 
 export default function NavSearch(props) {
   return (
-<div class="topnav">
-  <a class="active" href="#home">Home</a>
-  <a href="#about">About</a>
-  <a href="#contact">Contact</a>
-  <input type="text" placeholder="Search.."/>
-</div>
+        <>  <div class="container">
+                <div class="logo"></div>
+
+                 <div class="container2">
+                    <h2>Hacker News</h2>
+                        <div class="nav">
+                        <a href="#new">new</a>
+                        <a href="#">|</a>
+                        <a href="#past">past</a>
+                        <a href="#">|</a>
+                        <a href="#comments">comments</a>
+                        <a href="#">|</a>
+                        <a href="#ask">ask</a>
+                        <a href="#">|</a>
+                        <a href="#show">show</a>
+                        <a href="#">|</a>
+                        <a href="#jobs">jobs</a>
+                        <a href="#">|</a>
+                        <a href="#submit">submit</a>   
+                
+                        </div>
+
+                </div>
+                <div class="input">
+                   <input type="text" placeholder="Search.."/>
+                </div> 
+            </div>
+
+        </>   
   );
 }
